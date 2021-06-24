fd5a638c94081fa05559024660f20555c77d3327
Thach <thachdt4@gmail.com>
2021-06-24 15:56:54 +0200

    finalized setup script webserver

--- setup_webserver.py
+++ setup_webserver.py
@@ -14,7 +14,6 @@ def ip4_addresses():
 
 if __name__=="__main__":
 	print('setup IP address')
-	print(sys.argv[1])
 	iplist = ip4_addresses()
 	f = open('/home/ag-rossi/projects/duesselpore/NGS_webserver/settings.py', 'r').readlines()
 	f[27] = f[27][:-2]+','+ str(iplist)[1:] + '\n'
@@ -22,16 +21,29 @@ if __name__=="__main__":
 	f1.writelines(f)
 	f1.close()
 
-	print('Downloading human reference genome')
-	os.system('wget ftp://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz -P ~/ReferenceData/')
-	os.system('wget http://ftp.ensembl.org/pub/release-104/gtf/homo_sapiens/Homo_sapiens.GRCh38.104.gtf.gz -P ~/ReferenceData/')
-	print('creating human reference genome indexes, please wait')
-	os.system('minimap2 -t 4 -k15 -w10 -d ~/ReferenceData/reference_human.mmi ~/ReferenceData/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz')
-	install_cmd ='apt install fastqc'
-	sudo_pass = '123456'
-	os.system('echo %s|sudo -S %s'%(sudo_pass, install_cmd))
-	
+	if sys.argv[1]=='light':
+		print('Downloading human reference genome')
+		os.system('wget ftp://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz -P ~/ReferenceData/')
+		os.system('wget http://ftp.ensembl.org/pub/release-104/gtf/homo_sapiens/Homo_sapiens.GRCh38.104.gtf.gz -P ~/ReferenceData/')
+		print('creating human reference genome indexes, please wait')
+		os.system('minimap2 -t 4 -k15 -w10 -d ~/ReferenceData/reference_human.mmi ~/ReferenceData/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz')
+		install_cmd ='apt install fastqc'
+		sudo_pass = '123456'
+		os.system('echo %s|sudo -S %s'%(sudo_pass, install_cmd))
+		os.unlink('~/ReferenceData/*.fa.gz')
+
 	if sys.argv[1]=='full':
+		print('Downloading human reference genome')
+		os.system('wget ftp://ftp.ensembl.org/pub/release-104/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz -P ~/ReferenceData/')
+		os.system('wget http://ftp.ensembl.org/pub/release-104/gtf/homo_sapiens/Homo_sapiens.GRCh38.104.gtf.gz -P ~/ReferenceData/')
+		print('creating human reference genome indexes, please wait')
+		os.system('minimap2 -t 4 -k15 -w10 -d ~/ReferenceData/reference_human.mmi ~/ReferenceData/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz')
+		install_cmd ='apt install fastqc'
+		sudo_pass = '123456'
+		os.system('echo %s|sudo -S %s'%(sudo_pass, install_cmd))
+		os.unlink('~/ReferenceData/*.fa.gz')
+
+
 		print('Downloading mouse reference genome')
 		os.system('wget ftp://ftp.ensembl.org/pub/release-104/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.toplevel.fa.gz -P ~/ReferenceData/')
 		os.system('wget ftp://ftp.ensembl.org/pub/release-104/gtf/mus_musculus/Mus_musculus.GRCm39.104.gtf.gz -P ~/ReferenceData/')
@@ -56,3 +68,6 @@ if __name__=="__main__":
 		os.system('minimap2 -t 4 -k15 -w10 -d ~/ReferenceData/reference_zebrafish.mmi ~/ReferenceData/Danio_rerio.GRCz11.dna.toplevel.fa.gz')
 		print('creating C elegans reference genome indexes, please wait')
 		os.system('minimap2 -t 4 -k15 -w10 -d ~/ReferenceData/reference_celegans.mmi ~/ReferenceData/Caenorhabditis_elegans.WBcel235.dna.toplevel.fa.gz')
+		os.unlink('~/ReferenceData/*.fa.gz')
+
+	print('Your webserver IP address is %s, please use http://%s:8000/duesselpore/ on your browser:'%(iplist[-1], iplist[-1]))
