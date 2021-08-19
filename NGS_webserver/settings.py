cf12dc3b9c6a31604094c9fb910aaca8d19ddaf6
Thach <thachdt4@gmail.com>
2021-08-19 10:09:22 +0200

    changed timezone

--- NGS_webserver/settings.py
+++ NGS_webserver/settings.py
@@ -115,14 +115,12 @@ DEFAULT_FROM_EMAIL = 'your@email.com'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'MEZ'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
-
-
 
 STATIC_URL = '/static/'
